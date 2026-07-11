8e7bb80ca0079d7407a239fd5b0d75294dea142c
XilDen <makrusinanton8@gmail.com>
2026-07-11 23:31:22 +0700

    Update tg_handler.py

--- app/tg_handler.py
+++ app/tg_handler.py
@@ -3,17 +3,23 @@ from telegram.ext import Application
 from app.account_manager import AccountManager
 from app.tg_commands import register_handlers
 from app.time_utils import DEFAULT_APP_TIMEZONE
+from app.storage import Storage
+from app.config import Settings
 
 
 def build_tg_app(
     token: str,
     account_manager: AccountManager,
     admin_id: int,
+    storage: Storage,
+    settings: Settings,
     app_timezone: str = DEFAULT_APP_TIMEZONE,
 ) -> Application:
     app = Application.builder().token(token).build()
     app.bot_data["account_manager"] = account_manager
     app.bot_data["admin_id"] = int(admin_id)
     app.bot_data["app_timezone"] = app_timezone
+    app.bot_data["storage"] = storage
+    app.bot_data["settings"] = settings
     register_handlers(app)
     return app
